704ac3ee2f81383006d06f35664fb3f207d61bd9
Anwesh43 <anweshthecool0@gmail.com>
2019-01-17 09:50:48 +0530

    adding RtMorePlus class

--- rtmoreplusview/src/main/java/com/anwesh/uiprojects/rtmoreplusview/RtMorePlusView.kt
+++ rtmoreplusview/src/main/java/com/anwesh/uiprojects/rtmoreplusview/RtMorePlusView.kt
@@ -165,4 +165,28 @@ class RtMorePlusView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RtMorePlus(var i : Int) {
+
+        private var root : RMPNode = RMPNode(0)
+        private var curr : RMPNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
