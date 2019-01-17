44dd8af6a7a6b90a708b561dac1e8237bfc5f01b
Anwesh43 <anweshthecool0@gmail.com>
2019-01-17 10:01:32 +0530

    adding view in activity class

--- app/src/main/java/com/anwesh/uiprojects/linkedrtmoreplusview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrtmoreplusview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrtmoreplusview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rtmoreplusview.RtMorePlusView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RtMorePlusView.create(this)
     }
 }
