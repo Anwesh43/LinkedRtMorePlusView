de4688a3c9e92dc850d47c5caf9125b088f8f8f1
Anwesh43 <anweshthecool0@gmail.com>
2019-01-17 10:08:09 +0530

    adding delay as constant

--- rtmoreplusview/src/main/java/com/anwesh/uiprojects/rtmoreplusview/RtMorePlusView.kt
+++ rtmoreplusview/src/main/java/com/anwesh/uiprojects/rtmoreplusview/RtMorePlusView.kt
@@ -21,6 +21,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 3f
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -105,7 +106,7 @@ class RtMorePlusView(ctx : Context) : View(ctx) {
             if (animated) {
                 cb()
                 try {
-                    Thread.sleep(50)
+                    Thread.sleep(delay)
                     view.invalidate()
                 } catch(e : Exception) {
 
