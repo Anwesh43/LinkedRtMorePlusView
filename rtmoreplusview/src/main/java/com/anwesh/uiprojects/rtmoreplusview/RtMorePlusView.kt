f5446211571b38383daf502589419cd94b58ace6
Anwesh43 <anweshthecool0@gmail.com>
2019-01-17 09:28:43 +0530

    importing libraries and defining constants

--- rtmoreplusview/src/main/java/com/anwesh/uiprojects/rtmoreplusview/RtMorePlusView.kt
+++ rtmoreplusview/src/main/java/com/anwesh/uiprojects/rtmoreplusview/RtMorePlusView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.rtmoreplusview
  * Created by anweshmishra on 17/01/19.
  */
 
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 3f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
