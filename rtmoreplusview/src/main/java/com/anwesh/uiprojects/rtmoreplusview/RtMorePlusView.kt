931ae2707609a43cbef5fc34afadc8db24cdf4f7
Anwesh43 <anweshthecool0@gmail.com>
2019-01-17 09:38:35 +0530

    adding drawRMPNode canvas extension method

--- rtmoreplusview/src/main/java/com/anwesh/uiprojects/rtmoreplusview/RtMorePlusView.kt
+++ rtmoreplusview/src/main/java/com/anwesh/uiprojects/rtmoreplusview/RtMorePlusView.kt
@@ -28,3 +28,32 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Int, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawRTLine(x : Float, size : Float, deg : Float, paint : Paint) {
+    save()
+    translate(x, 0f)
+    rotate(deg)
+    drawLine(0f, 0f, size, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawRMPNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = foreColor
+    save()
+    translate(w/2, gap * (i + 1))
+    rotate(90f * sc2)
+    drawRect(RectF(-size/2, -size/2, size/2, size/2), paint)
+    for (j in 0..(lines - 1)) {
+        val sc : Float = sc1.divideScale(j, lines)
+        drawRTLine(size / 2, size/2, 45f * sc, paint)
+    }
+    restore()
+}
